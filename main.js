0e31139a6a4395fafefea2f36f54cd46d640f9ff
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-06-02 12:35:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// PRIMER EJERCICIO 
+// ****Entrada****//
+let nombre = prompt('Ingrese su nombre')
+let apellido = prompt('Ingrese su apellido')
+// ****Proceso****//
+let mensaje = `Hola que tal, mi nombre es ${nombre}, y mi apellido es ${apellido}`
+//****Salida****//
+console.log(mensaje)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
